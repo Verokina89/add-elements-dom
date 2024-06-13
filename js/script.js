d41c5df25b4b5a02171f6907511d9733dbda2652
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-06-13 21:54:15 +0200

    primera subida ejercicio 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
- // Aquí tu código
+ // Aquí tu código.
+ //lista--> donde tiene que salir lo elementos
+ //agregar --> boton click 
+
+const botonAceptar = document.getElementById("Aceptar");
+const lista = document.getElementById("lista")
+
+
+function addElement () {
+    const tareas = prompt("ingresa tu nombre")
+    if(tareas) {
+      
+       lista.insertAdjacentHTML("beforeend", "<li><p>"+ tareas + "</li></p>")
+       
+    } else {
+        alert("Tienes que ingresar algún dato")
+    }
+};
+
+botonAceptar.addEventListener("botonAceptar", addElement);
+
+
+
+
+
+  
+
